0b0d74322325039f66459dd5f23f50b6800841ce
Joao Castro <joao.castro.93@hotmail.com>
2024-05-07 16:31:37 +0100

    fix name

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -10,7 +10,7 @@ export function Post() {
     <div className="container">
       <h1 className="page-title">{post.title}</h1>
       <span className="page-subtitle">
-        By: <Link to={`/users/${post.userId}`}>{user.username}</Link>
+        By: <Link to={`/users/${post.userId}`}>{user.name}</Link>
       </span>
       <div>{post.body}</div>
       <h3 className="mt-4 mb-2">Comments</h3>
